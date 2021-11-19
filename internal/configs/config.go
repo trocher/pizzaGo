9d716256f2a1f4d3207dd94dfb27aa6ff7ba797b
trocher <tanguy.rocher@epfl.ch>
2021-11-19 15:00:54 +0100

    modified configs types

--- internal/configs/config.go
+++ internal/configs/config.go
@@ -1,20 +1,20 @@
 package configs
 
 type TimingsConfig struct {
-	Process      uint
-	Prepare      uint
-	Bake         uint
-	QualityCheck uint
+	Process      uint64
+	Prepare      uint64
+	Bake         uint64
+	QualityCheck uint64
 }
 
 var Timings = TimingsConfig{1, 2, 5, 1}
 
 type HyperParameters struct {
-	NumberOfWorker int
+	NumberOfWorker uint64
 	NumberOfOven   uint64
 	NumberOfOrder  uint64
 }
 
-var Parameters = HyperParameters{2, 4, 500}
+var Parameters = HyperParameters{2, 2, 500}
 
-var SlowFactor = 0.3
+var SlowFactor uint64 = 1
