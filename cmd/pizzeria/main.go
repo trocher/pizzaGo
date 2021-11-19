b839b4e11048d8fa30feadfdf05567c52bac0221
trocher <tanguy.rocher@epfl.ch>
2021-11-19 15:00:19 +0100

    moved pizzaBaker creation to the pizzeria file

--- cmd/pizzeria/main.go
+++ cmd/pizzeria/main.go
@@ -1,38 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"pizzago/internal/components"
 	"pizzago/internal/configs"
-	"strconv"
-	"sync"
+	"pizzago/internal/timing"
 	"time"
 )
 
 func main() {
-	fmt.Println("Starting pizzago")
+	log.Printf("Starting pizzago ...")
 
-	pizzaWorkers := make([]components.PizzaWorker, configs.Parameters.NumberOfWorker)
-	fmt.Println("There are " + strconv.Itoa(configs.Parameters.NumberOfWorker) + " workers today")
-
-	components.InitOven()
-	var wg sync.WaitGroup
-
-	for i := range pizzaWorkers {
-		fmt.Println("Waking up worker " + strconv.Itoa(i))
-		pizzaWorkers[i] = components.PizzaWorker{Name: uint64(i + 1), HasAssignedOven: false}
-		wg.Add(1)
-	}
-	log.Printf("Starting the pizzeria")
 	start := time.Now()
-
-	for _, pizzaWorker := range pizzaWorkers {
-		go pizzaWorker.Work(&wg)
-	}
-	wg.Wait()
-
+	components.StartPizzeria()
 	elapsed := time.Since(start)
+
 	log.Printf("Took %s to cook %d pizzas with %d workers and %d ovens", elapsed, configs.Parameters.NumberOfOrder, configs.Parameters.NumberOfWorker, configs.Parameters.NumberOfOven)
-	log.Printf("%d order were taken and %d were delivered", components.OrderTaken, components.OrderDelivered)
+
+	expectedTime := timing.ExpectedTime()
+
+	log.Printf("overhead was %s (%d%%)as time taken is %s and expected time would be %s ", elapsed-expectedTime, elapsed-expectedTime/expectedTime, elapsed, expectedTime)
 }
--- internal/components/pizzeria.go
+++ internal/components/pizzeria.go
@@ -1,7 +1,11 @@
 package components
 
 import (
+	"fmt"
+	"log"
 	"pizzago/internal/configs"
+	"strconv"
+	"sync"
 )
 
 // === Pizza definition ===
@@ -10,9 +14,31 @@ type Pizza struct {
 }
 
 var ovenList = make([]PizzaOven, configs.Parameters.NumberOfOven)
+var pizzaWorkers = make([]PizzaWorker, configs.Parameters.NumberOfWorker)
 
 func InitOven() {
 	for i := range ovenList {
 		ovenList[i] = PizzaOven{isUsed: 0}
 	}
+}
+
+func InitBakers() {
+	for i := range pizzaWorkers {
+		fmt.Println("Waking up worker " + strconv.Itoa(i))
+		pizzaWorkers[i] = PizzaWorker{Name: uint64(i + 1), HasAssignedOven: false}
+	}
+}
+
+func StartPizzeria() {
+	InitOven()
+	InitBakers()
+	var wg sync.WaitGroup
+	log.Printf("Starting the pizzeria")
+
+	for _, pizzaWorker := range pizzaWorkers {
+		wg.Add(1)
+		go pizzaWorker.Work(&wg)
+	}
+	wg.Wait()
+
 }
