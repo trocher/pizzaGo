1394c5643ecbbfe9518b280d9a797350a8521c1f
trocher <tanguy.rocher@epfl.ch>
2021-11-19 15:00:37 +0100

    added the expected time computation

--- internal/timing/timing.go
+++ internal/timing/timing.go
@@ -5,6 +5,15 @@ import (
 	"time"
 )
 
-func WaitFor(t uint) {
+func WaitFor(t uint64) {
 	time.Sleep(time.Duration(t) * time.Duration(configs.SlowFactor) * time.Millisecond)
+}
+
+func ExpectedTime() time.Duration {
+	bottleneck := configs.Parameters.NumberOfWorker
+	if bottleneck > configs.Parameters.NumberOfOven {
+		bottleneck = configs.Parameters.NumberOfOven
+	}
+	expected := (configs.Timings.Bake + configs.Timings.Prepare + configs.Timings.Process + configs.Timings.QualityCheck) * configs.SlowFactor * configs.Parameters.NumberOfOrder / bottleneck
+	return time.Duration(expected) * time.Duration(time.Millisecond)
 }
